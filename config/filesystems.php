a7c7795a7a8a2b0a400d29e017342e59b76c6e4c
Farino Joshua <96770446+farinojoshua@users.noreply.github.com>
2023-08-05 20:37:53 +0700

    add

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,9 +38,10 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path(),
-            'url' => env('APP_URL').'/public',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
+            'throw' => false,
         ],
 
         's3' => [
